4b03c65f8f16e4bcba0c702b8791e54fd28c3ee7
Anthony Iroegbu <anthony@crenettechlabs.com>
2022-01-26 16:20:41 +0100

    storing and retrieving values from imported contracts

--- StorageFactory.sol
+++ StorageFactory.sol
@@ -6,9 +6,30 @@ import "./SimpleStorage.sol";
 
 contract StorageFactory {
     SimpleStorage[] public simpleStorageArray;
-    
+
     function createSimpleStorageContact() public {
         SimpleStorage simpleStorage = new SimpleStorage();
         simpleStorageArray.push(simpleStorage);
+    }
+
+    function storageFactoryStore(
+        uint256 _simpleStorageIndex,
+        uint256 _simpleStorageNumber
+    ) public {
+        SimpleStorage simpleStorage = SimpleStorage(
+            address(simpleStorageArray[_simpleStorageIndex])
+        );
+        simpleStorage.store(_simpleStorageNumber);
+    }
+
+    function storageFactoryGet(uint256 _simpleStorageIndex)
+        public
+        view
+        returns (uint256)
+    {
+        SimpleStorage simpleStorage = SimpleStorage(
+            address(simpleStorageArray[_simpleStorageIndex])
+        );
+        return simpleStorage.retreive();
     }
 }
